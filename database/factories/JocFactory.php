d411238d71a5c530a5faf055bc77af93e3a5c233
Gerard Aguilar <gerard.aguilar1@insbaixcamp.cat>
2025-11-02 18:21:04 +0100

    Prova2

--- database/factories/JocFactory.php
+++ database/factories/JocFactory.php
@@ -20,7 +20,7 @@ class JocFactory extends Factory
             'nom' => $this->faker->words(2, true),
 
             'estudi' => $this->faker->randomElement([
-                'Ubisoft', 'FromSoftware', 'Electronic Arts', 'Square Enix', 'Capcom', 'Valve', 'Bethesda', 'CD Projekt'
+                'Nintendo', 'Ubisoft', 'FromSoftware', 'Electronic Arts', 'Square Enix', 'Capcom', 'Valve', 'Bethesda', 'CD Projekt'
             ]),
 
             'data_publicacio' => $this->faker->date($format = 'Y-m-d', $max = 'now'),
