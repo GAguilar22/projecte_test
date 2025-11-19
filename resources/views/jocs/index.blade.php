6641408bc946922387ecd58e1e81c36e9a540993
Gerard Aguilar <gerard.aguilar1@insbaixcamp.cat>
2025-11-19 20:24:33 +0100

    Fix: Restaurar boton crear + fondo azul cielo con CSS inline

--- resources/views/jocs/index.blade.php
+++ resources/views/jocs/index.blade.php
@@ -5,6 +5,9 @@
 @section('content')
     <div class="d-flex justify-content-between align-items-center mb-3">
         <h1 class="h3 mb-0">Jocs</h1>
+        <a href="{{ route('jocs.create') }}" class="btn btn-primary">
+            <i class="fas fa-plus me-1"></i> Crear Joc
+        </a>
     </div>
 
     @if(session('success'))
